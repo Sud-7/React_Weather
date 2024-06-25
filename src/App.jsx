02a6ff504ad73981f3f7fd384cc3bea3c85f1c88
Sud <155895839+Sud-7@users.noreply.github.com>
2024-06-25 14:16:15 +0530

    Enter entered

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import TopButton from "./components/TopButtons";
 import Inputs from "./components/Inputs";
 import TimeandLocation from "./components/TimeandLocation";
@@ -8,63 +7,61 @@ import getWeatherData from "./services/weatherServices";
 import getFormattedWeatherData from "./services/weatherServices";
 import { useEffect, useState } from "react";
 
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
-function capitalizeFirstLetter (string) {
-  return string.charAt(0).toUpperCase() + string.slice(1)
- }
-
+function capitalizeFirstLetter(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
 function App() {
-
-  const [query, setQuery] = useState({q: 'Noida'})
-  const [units, setUnits] = useState('metric')
-  const [weather, setWeather] = useState(null)
-
+  const [query, setQuery] = useState({ q: "Noida" });
+  const [units, setUnits] = useState("metric");
+  const [weather, setWeather] = useState(null);
 
   const getWeather = async () => {
+    const message = query.q ? query.q : "current Location";
+    toast.info(`Fetching weather for ${capitalizeFirstLetter(message)}`);
 
-    const message = query.q ? query.q :'current Location';
-    toast.info(`Fetching weather for ${capitalizeFirstLetter(message)}`)
-    
-    await getFormattedWeatherData({...query, units}).then(data => {
-      toast.success(`Fetched waeather for ${data.name}, ${data.country}`)
-      setWeather(data)})
+    await getFormattedWeatherData({ ...query, units }).then((data) => {
+      toast.success(`Fetched waeather for ${data.name}, ${data.country}`);
+      setWeather(data);
+    });
 
     // console.log(data)
-  }
+  };
 
   useEffect(() => {
-    getWeather()
-  }, [query, units])
+    getWeather();
+  }, [query, units]);
 
   const formatBackground = () => {
-    if (!weather) return "from-cyan-600 to-blue-700"
-    const threshold = units === 'metric' ? 20 : 60;
+    if (!weather) return "from-cyan-600 to-blue-700";
+    const threshold = units === "metric" ? 20 : 60;
     if (weather.temp <= threshold) return "from-cyan-600 to-blue-700";
-    return "from-yellow-600 to-orange-700"
-  }
+    return "from-yellow-600 to-orange-700";
+  };
 
   return (
     <div className="bg-gradient-to-br from-cyan-600 to-blue-700">
-      <div className={`mx-auto mt-5 max-w-screen-lg  py-5 px-32 bg-gradient-to-br shadow-gray-400 ${formatBackground()}`}>
-      <TopButton setQuery={setQuery} />
-      <Inputs setQuery={setQuery} setUnits={setUnits}  />
+      <div
+        className={`mx-auto max-w-screen-lg  py-5 px-32 bg-gradient-to-br shadow-gray-400 ${formatBackground()}`}
+      >
+        <TopButton setQuery={setQuery} />
+        <Inputs setQuery={setQuery} setUnits={setUnits} />
 
-      {weather && (
-        <>
-          <TimeandLocation weather={weather} />
-          <TempreatureandDetails  weather={weather} units={units}/>
-          <Forecast title={'3 Hour Step Forecast'} data={weather.hourly}/>
-          <Forecast title={'Daily Forecast'} data={weather.daily}/>
-        </>
-      )}
+        {weather && (
+          <>
+            <TimeandLocation weather={weather} />
+            <TempreatureandDetails weather={weather} units={units} />
+            <Forecast title={"3 Hour Step Forecast"} data={weather.hourly} />
+            <Forecast title={"Daily Forecast"} data={weather.daily} />
+          </>
+        )}
+      </div>
+      <ToastContainer autoClose={2500} hideProgressBar={true} theme="colored" />
     </div>
-    <ToastContainer autoClose={2500} hideProgressBar={true} theme="colored" />
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,55 +1,68 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
-import { BiCurrentLocation, BiSearch } from 'react-icons/bi'
+import { BiCurrentLocation, BiSearch } from "react-icons/bi";
 
-function Inputs({setQuery, setUnits}) {
+function Inputs({ setQuery, setUnits }) {
+  const [city, setCity] = useState("");
 
-    const [city, setCity] = useState("")
+  const handleSearchClick = () => {
+    if (city !== "") setQuery({ q: city });
+  };
 
-    const handleSearchClick = () => {
-        if(city !== "") setQuery({q: city})
+  const handleLocationClick = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        setQuery({ lat: latitude, lon: longitude });
+      });
     }
-
-    const handleLocationClick = () => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition((position) => {
-                const {latitude, longitude} = position.coords
-                setQuery({lat: latitude, lon: longitude})
-            })
-        }
-    }
+  };
 
   return (
-    <div className='flex flex-row justify-center my-6'>
-
-        <div className='flex flex-row w-3/4 items-center justify-center space-x-4'>
-          <input 
-              type='text'
-              value={city}
-              onChange={(e) => setCity(e.currentTarget.value)}
-              placeholder='search for city...'
-              className='text-xl rounded-md font-light p-2 w-full shadow-xl capitalize placeholder:lowercase focus:outline-none '
-          />
-          <BiSearch 
-            size={40}
-            onClick={handleSearchClick}
-            className='text-white cursor-pointer transition ease-out hover:scale-125'  
-          />
-          <BiCurrentLocation
+    <div className="flex flex-row justify-center my-6">
+      <div className="flex flex-row w-3/4 items-center justify-center space-x-4">
+        <input
+          type="text"
+          value={city}
+          onChange={(e) => setCity(e.currentTarget.value)}
+          onKeyDown={(e) => {
+            console.log(e.keyCode);
+            if (e.keyCode == 13) handleSearchClick();
+          }}
+          placeholder="search for city..."
+          className="text-xl rounded-md font-light p-2 w-full shadow-xl capitalize placeholder:lowercase focus:outline-none "
+        />
+        <BiSearch
+          size={40}
+          onClick={handleSearchClick}
+          className="text-white cursor-pointer transition ease-out hover:scale-125"
+        />
+        <BiCurrentLocation
           onClick={handleLocationClick}
-            size={40}
-            className='text-white cursor-pointer transition ease-out hover:scale-125'
-          />
-        </div>
+          size={40}
+          className="text-white cursor-pointer transition ease-out hover:scale-125"
+        />
+      </div>
 
-        <div className='flex flex-row w-1/4 items-center justify-center space-x-2' >
-          <button name='metric' className='text-2xl text-white font-light hover:scale-125' onClick={() => setUnits("metric")}  >°C</button> 
-          <p className='text-2xl text-white mx-1' >|</p>
-          <button name='imperial' className='text-2xl text-white font-light hover:scale-125' onClick={() => setUnits("imperial")} >°F</button>      
-        </div>
-
+      <div className="flex flex-row w-1/4 items-center justify-center space-x-2">
+        <button
+          name="metric"
+          className="text-2xl text-white font-light hover:scale-125"
+          onClick={() => setUnits("metric")}
+        >
+          °C
+        </button>
+        <p className="text-2xl text-white mx-1">|</p>
+        <button
+          name="imperial"
+          className="text-2xl text-white font-light hover:scale-125"
+          onClick={() => setUnits("imperial")}
+        >
+          °F
+        </button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Inputs
+export default Inputs;
